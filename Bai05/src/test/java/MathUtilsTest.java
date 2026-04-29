8696b509f5927331311ac2690f07d0e92efdfd29
ducquangdd050607 <duongdinhducquang567@gmail.com>
2026-04-29 23:16:04 +0700

    Fixing test coverage

--- Bai05/src/test/java/MathUtilsTest.java
+++ Bai05/src/test/java/MathUtilsTest.java
@@ -6,6 +6,10 @@ public class MathUtilsTest {
     public void testAdd() {
         MathUtils math = new MathUtils();
         // chỉ test hàm add()
-        assertEquals(5, math.add(2, 3)); 
+        assertEquals(5, math.add(2, 3));
+        //thêm test các hàm khác 
+        assertEquals(1, math.subtract(3, 2));
+        assertEquals(6, math.multiply(2, 3));
+        assertEquals(2, math.divide(6, 3));
     }
 }
