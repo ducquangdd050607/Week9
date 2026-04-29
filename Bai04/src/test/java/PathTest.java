2034da1a67b3ac9f6b37852accfd15b0966b7e9a
ducquangdd050607 <duongdinhducquang567@gmail.com>
2026-04-29 22:55:05 +0700

    Test đúng với mọi os

--- Bai04/src/test/java/PathTest.java
+++ Bai04/src/test/java/PathTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PathTest {
@@ -7,15 +10,18 @@ public class PathTest {
     @Test
     public void testHardcodedFilePath() {
         // Cố tình dùng định dạng cứng của Windows (dấu \ )
+        /*
         String logPath = "var\\log\\app.log";
         
         File file = new File(logPath);
-        String fileName = file.getName(); // Lấy tên tệp từ đường dẫn
+        String fileName = file.getName();
         
-        // KIẾN THỨC: 
-        // - Trên Windows: Máy hiểu '\' là phân cách thư mục, nên nó tách được tên file là "app.log". Test PASS.
-        // - Trên Linux/Mac: Máy chỉ hiểu '/' là phân cách. Nó coi toàn bộ chuỗi "var\log\app.log" 
-        //   là TÊN CỦA 1 FILE DUY NHẤT. Do đó getName() trả về "var\log\app.log". Test FAIL.
-        assertEquals("app.log", fileName, "Tên file trích xuất bị sai do lỗi hệ điều hành!");
+        assertEquals("app.log", fileName, "Tên file trích xuất bị sai do lỗi hệ điều hành!");*/
+        Path logPath = Paths.get("var", "log", "app.log");
+        
+        String fileName = logPath.getFileName().toString();
+        
+        assertEquals("app.log", fileName, "Tên file trích xuất phải luôn là app.log trên mọi HĐH");
+
     }
 }
